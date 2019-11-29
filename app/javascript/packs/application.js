5021c0ca415c860b55ecba79b9523e1e22e8a3cf
alexbispo <alexbispo.artes@gmail.com>
2019-11-29 02:45:51 -0200

    Melhorando o layout da pagina novo apontamento.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,8 +13,6 @@ require("bootstrap/dist/js/bootstrap")
 require("../stylesheets/application")
 require("@fortawesome/fontawesome-free/js/all")
 
-require("packs/apontamento-form")
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
--- config/initializers/date_formats.rb
+++ config/initializers/date_formats.rb
@@ -1,1 +1,2 @@
 Time::DATE_FORMATS[:apontamento] = "%d/%m/%Y %H:%M"
+Time::DATE_FORMATS[:input_datetime_local] = "%Y-%m-%dT%H:%M"
